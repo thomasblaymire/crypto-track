c7ea7018984c240eee6ced0d30648bcdd0353180
Tom <thomasblaymire@gamil.com>
2022-01-01 20:14:52 +1100

    feat(app): improve webpack setup including hot module replacement and public path

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,10 @@
 import path from 'path';
+import webpack from 'webpack';
 import { getSVGLoader } from './webpack-helpers';
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const port = process.env.PORT || 4000;
 
 const createWebpackConfig = async () => {
   return {
@@ -24,18 +29,33 @@ const createWebpackConfig = async () => {
         getSVGLoader(),
       ],
     },
-    devtool: 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new CopyPlugin({
+        patterns: [{ from: 'src/assets', to: '' }],
+      }),
+    ],
+    devtool: 'inline-source-map',
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom',
+      },
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
+      filename: 'bundle.[fullhash].js',
     },
     devServer: {
       static: path.join(__dirname, 'dist'),
       compress: true,
-      port: 4000,
+      port: port,
+      historyApiFallback: true,
+      open: true,
+      hot: true,
     },
   };
 };
