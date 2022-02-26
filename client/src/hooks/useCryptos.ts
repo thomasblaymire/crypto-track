31633d25c4c2520abdce5c81fc2663f06629a9e4
Tom <thomasblaymire@gamil.com>
2022-02-26 23:11:59 +1100

    feat(client): add in query cancellation to search along with API retry

--- client/src/hooks/useCryptos.ts
+++ client/src/hooks/useCryptos.ts
@@ -3,11 +3,18 @@ import { ALL_COIN_QUERY_STRING } from '../constants';
 
 // A simple data fetching hook used to fetch all cryptos based on a given users currency.
 export const useCryptos = (currency?: string) => {
-  return useQuery('cryptos', async () => {
-    const response = await fetch(
-      `${process.env.COINGEKO_API}/${ALL_COIN_QUERY_STRING(currency)}`
-    );
-    const data = await response.json();
-    return data;
-  });
+  return useQuery(
+    'cryptos',
+    async () => {
+      const response = await fetch(
+        `${process.env.COINGEKO_API}/${ALL_COIN_QUERY_STRING(currency)}`
+      );
+
+      const data = await response.json();
+      return data;
+    },
+    {
+      retry: 5,
+    }
+  );
 };
--- client/src/hooks/useSearch.ts
+++ client/src/hooks/useSearch.ts
@@ -1,17 +1,43 @@
 import { useQuery } from 'react-query';
 
-// A simple search hook used to fetch all cryptos based on a given string.
+interface CancellablePromise<T> extends Promise<T> {
+  cancel: () => void;
+}
+
+type QueryPromise = CancellablePromise<string | boolean>;
+
+// In order to save bandwidth I decided to implement query cancellation with promises,
+// therefore when a search query is cancelled e.g from a 404 the browser will not download the data. #perf
 export const useSearch = (query?: string) => {
   return useQuery(
     ['query', query],
     async () => {
-      const response = await fetch(
-        `${process.env.COINGEKO_API}/search?query=${query}`
-      );
-      const data = await response.json();
-      return data.coins.slice(0, 5);
+      const controller = new AbortController();
+      const signal = controller.signal;
+
+      const promise: Partial<QueryPromise> = new Promise<void>(resolve =>
+        resolve()
+      )
+        .then(() => {
+          return fetch(`${process.env.COINGEKO_API}/search?query=${query}`, {
+            method: 'GET',
+            signal,
+          });
+        })
+        .then(res => {
+          return res.json();
+        })
+        .then(data => data.coins.slice(0, 5));
+
+      promise.cancel = () => {
+        controller.abort();
+      };
+
+      return promise as QueryPromise;
     },
+
     {
+      retry: 2,
       enabled: true,
     }
   );
