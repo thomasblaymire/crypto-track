6f24ec79e689b6a0f7f01055bb26d35165d709d6
Tom Blaymire <thomasblaymire@gmail.com>
2022-05-02 20:10:11 +1000

    refactor(api): update production error operational vs not

--- api/src/middlewares/error-handler.ts
+++ api/src/middlewares/error-handler.ts
@@ -10,10 +10,22 @@ const sendDevelopmentError = (err: any, res: Response) => {
 };
 
 const sendProductionError = (err: any, res: Response) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // Log basic error
+    console.error('ERROR 💥:', err);
+    // Send generic messag
+    res.status(500).json({
+      status: 'error',
+      message: 'Somewhing went wrong',
+    });
+  }
 };
 
 export const globalErrorHandler = (err: any, res: Response, next: NextFunction) => {
