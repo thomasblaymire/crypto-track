a6ca7beef9fb7f3c6ea8fa2998e00fdba92ac679
Tom Blaymire <thomasblaymire@gmail.com>
2022-05-02 20:02:17 +1000

    refactor(api): seperate development errors to production errors

--- api/src/middlewares/error-handler.ts
+++ api/src/middlewares/error-handler.ts
@@ -1,11 +1,28 @@
 import { Response, NextFunction } from 'express';
 
-export const globalErrorHandler = (err: any, res: Response, next: NextFunction) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendDevelopmentError = (err: any, res: Response) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const sendProductionError = (err: any, res: Response) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+export const globalErrorHandler = (err: any, res: Response, next: NextFunction) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendDevelopmentError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProductionError(err, res);
+  }
 };
