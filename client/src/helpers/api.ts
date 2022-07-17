7974339be4b6a66e4923d469b46ccf48cdf21b91
Tom Blaymire <thomasblaymire@gmail.com>
2022-07-17 09:33:41 +0200

    feat(app) refactor api to correctly use async await

--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,23 +1,5 @@
-// The idea of this file is to provide a centralized set of helpers that we can all when we want to pass data to a specific endpoint.
-
-const SINGLE_COIN_QUERY_STRING = (crypto?: string): string =>
-  `coins/${crypto}?localization=false&tickers=false&market_data=false&developer_data=false&sparkline=false`;
-
-const ALL_COIN_QUERY_STRING = (currency?: string, page?: number): string => {
-  // Currency will eventually be stored on userData with backend, temporarily lets use localStorage.
-  // const defaultCurrency = currency || 'gbp';
-  // const userCurrency = window.localStorage.getItem('currency');
-
-  // if (userCurrency) {
-  //   currency = userCurrency;
-  // }
-
-  const defaultCurrency = window.localStorage.getItem('currency') || 'gbp';
-
-  return `api/cryptos/${defaultCurrency}/${page}`;
-};
-
 import { storage } from './storage';
+import { API_URL } from '../constants';
 
 interface AuthResponse {
   user: User;
@@ -31,47 +13,56 @@ export interface User {
   token?: string;
 }
 
-const API_URL = 'http://127.0.0.1:3000/api';
-
-export async function handleApiResponse(response) {
+async function handleApiResponse(response): Promise<any> {
   const data = await response.json();
   if (response.ok) {
     return data;
-  } else {
-    return Promise.reject(data);
   }
+  return Promise.reject(data);
 }
 
-export async function getUserProfile() {
-  return await fetch(`${API_URL}/users/me`, {
+async function getUserProfile(): Promise<AuthResponse> {
+  const response = await window.fetch(`${API_URL}/users/me`, {
     headers: {
       Authorization: `Bearer ${storage.getToken()}`,
     },
-  }).then();
+  });
+  return handleApiResponse(response);
 }
 
-export async function loginWithEmailAndPassword(data): Promise<AuthResponse> {
-  return window
-    .fetch(`${API_URL}/users/signin`, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      credentials: 'include',
-      headers: {
-        'Content-type': 'application/json',
-      },
-    })
-    .then(handleApiResponse);
+async function loginWithEmailAndPassword(data): Promise<AuthResponse> {
+  const response = await window.fetch(`${API_URL}/users/signin`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    credentials: 'include',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  return handleApiResponse(response);
 }
 
-export async function registerWithEmailAndPassword(
-  data
-): Promise<AuthResponse> {
-  return window
-    .fetch(`${API_URL}/auth/register`, {
-      method: 'POST',
-      body: JSON.stringify(data),
-    })
-    .then(handleApiResponse);
+async function registerWithEmailAndPassword(data): Promise<AuthResponse> {
+  const response = await window.fetch(`${API_URL}/users/signup`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+
+  return handleApiResponse(response);
 }
 
-export { ALL_COIN_QUERY_STRING, SINGLE_COIN_QUERY_STRING };
+async function storeUserCurrency(data): Promise<AuthResponse> {
+  const response = await window.fetch(`${API_URL}/users/currency`, {
+    method: 'PATCH',
+    body: JSON.stringify(data),
+  });
+  return handleApiResponse(response);
+}
+
+export {
+  storeUserCurrency,
+  registerWithEmailAndPassword,
+  loginWithEmailAndPassword,
+  getUserProfile,
+  handleApiResponse,
+};
